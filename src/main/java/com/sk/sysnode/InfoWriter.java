2ed6b5870e8ae8e7f7e0444f39b7a7055962a5cf
Sameer Kulkarni <s.kulkarni@tignum.com>
2021-01-21 10:50:08 +0100

    updated propertyname

--- src/main/java/com/sk/sysnode/InfoWriter.java
+++ src/main/java/com/sk/sysnode/InfoWriter.java
@@ -1,4 +1,4 @@
-package com.tignum.sysnode;
+package com.sk.sysnode;
 
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
@@ -16,35 +16,6 @@ import java.util.logging.Logger;
 public class InfoWriter implements Plugin<Project> {
     private final static Logger LOGGER = Logger.getLogger(InfoWriter.class.getName());
 
-    /*
-    /*class InfoWriter implements Plugin<Project> {
-    @Override
-    void apply(Project project) {
-        project.with {
-            task("info", description: "Gets Sysnode info", group: "other") {
-                doLast {
-                    new File("$buildDir/resources/main/ms.properties").withWriter { w ->
-                        def cmd = "git rev-parse --short HEAD"
-                        def proc = cmd.execute()
-                        ext.revision = proc.text.trim()
-                        ext.timestamp = (int) (new Date().getTime() / 1000)
-
-                        Properties p = new Properties()
-                        p['prj.version'] = project.version.toString()
-                        p['prj.buildTime'] = Instant.now().toString()
-                        p['prj.revision'] = ext.revision.toString()
-                        def dateUnix = ext.timestamp
-                        Date dateObj = new Date(((long) dateUnix) * 1000)
-                        def cleanDate = new SimpleDateFormat('yyyy-MM-dd HH:mm').format(dateObj)
-                        p['prj.revisionTime'] = cleanDate
-
-                        p.store w, null
-                    }
-                }
-            }
-        }
-    }
-}*/
     @Override
     public void apply(final Project project) {
         project.task("info", new Action<Task>() {
@@ -65,7 +36,7 @@ public class InfoWriter implements Plugin<Project> {
                     }
                     properties.put("prj.buildTime", Instant.now().toString());
                     properties.put("core.version", project.findProperty("coreversion"));
-                    properties.put("group", project.findProperty("group"));
+                    properties.put("prj.group", project.findProperty("group"));
                     properties.store(fr, "Properties");
                     fr.close();
                 } catch (Exception e) {
