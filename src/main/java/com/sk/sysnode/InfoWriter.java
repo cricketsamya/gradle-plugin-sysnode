2f664340b15efb4228264791ac2afd28dcd4ac4e
Sameer Kulkarni <s.kulkarni@tignum.com>
2021-01-21 10:44:40 +0100

    removed code

--- src/main/java/com/sk/sysnode/InfoWriter.java
+++ src/main/java/com/sk/sysnode/InfoWriter.java
@@ -5,11 +5,17 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.time.Instant;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class InfoWriter implements Plugin<Project> {
+    private final static Logger LOGGER = Logger.getLogger(InfoWriter.class.getName());
+
     /*
     /*class InfoWriter implements Plugin<Project> {
     @Override
@@ -40,22 +46,31 @@ public class InfoWriter implements Plugin<Project> {
     }
 }*/
     @Override
-    public void apply(Project project) {
-        Task task = project.getTasks().create("info");
-        task.doLast(new Action<Task>() {
+    public void apply(final Project project) {
+        project.task("info", new Action<Task>() {
             @Override
             public void execute(Task task) {
                 try {
-                    System.out.println(project.getBuildDir().getAbsolutePath());
+                    final FileOutputStream fr = new FileOutputStream(project.getProjectDir().getAbsolutePath() + new File("/ms.properties"));
                     final Properties properties = new Properties();
+                    String cmd = "git rev-parse --short HEAD";
+                    Runtime run = Runtime.getRuntime();
+                    Process pr = run.exec(cmd);
+                    pr.waitFor();
+                    BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+                    String line = "";
                     properties.put("prj.version", project.getVersion());
-                    final FileOutputStream fr = new FileOutputStream(new File(project.getBuildDir().getAbsolutePath() + "/ms.properties"));
+                    while ((line = buf.readLine()) != null) {
+                        properties.put("prj.revision", line.trim());
+                    }
+                    properties.put("prj.buildTime", Instant.now().toString());
+                    properties.put("core.version", project.findProperty("coreversion"));
+                    properties.put("group", project.findProperty("group"));
                     properties.store(fr, "Properties");
                     fr.close();
                 } catch (Exception e) {
-                    System.out.println("ERROR : " + e.getMessage());
+                    LOGGER.severe("Error while generating ms.properties " + e.getMessage());
                 }
-                System.out.println(project.getVersion());
             }
         });
     }
